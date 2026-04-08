e201f9009d2032f0e2900e79cf4677ab16103e4b
BruceChia <2136414704@qq.com>
2026-04-08 21:54:13 +0800

    fix: tidy import spacing

--- src/wiki2md/cli.py
+++ src/wiki2md/cli.py
@@ -1,6 +1,5 @@
 import typer
 
-
 app = typer.Typer(
     add_completion=False,
     no_args_is_help=True,
--- tests/test_cli_smoke.py
+++ tests/test_cli_smoke.py
@@ -2,7 +2,6 @@ from typer.testing import CliRunner
 
 from wiki2md.cli import app
 
-
 runner = CliRunner()
 
 
